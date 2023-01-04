53610f5aa3ac5012258a5e1284a0540a51abeddb
macOS Monterey <macosmonterey@macOSs-MacBook-Pro.local>
2023-01-04 16:39:18 +0200

    DZ-15

--- dz15/index.js
+++ dz15/index.js
@@ -5,6 +5,12 @@ function getPow(num, degree) {
   return (degree === 1) ? num : num * (getPow(num, degree - 1));
 }
 
+// function getPow(num, degree) {
+//   if (degree === 1) {
+//     return num;
+//   }
+//   return num * (getPow(num, degree - 1));
+// }
 const userNumber = prompt('Введіть число');
 const userDegree = prompt('Введіть степень');
 
